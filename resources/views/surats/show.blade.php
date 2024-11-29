cda0a3696a49ddfd94f73057db9fa5fd8eef7f28
Daffa Raka <daffarakals@gmail.com>
2024-11-29 09:15:05 +0700

    Perubahan show

--- resources/views/surats/show.blade.php
+++ resources/views/surats/show.blade.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Detail Surat</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
 </head>
 <body>
-    <div class="container mt-5">
+    <div class="container my-5">
         <h1>Detail Surat</h1>
         <p><strong>Nomor Surat:</strong> {{ $surat->nomor_surat }}</p>
         <p><strong>Kategori:</strong> {{ $surat->kategori }}</p>
